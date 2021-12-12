fe70ac02d2fe0a001e57b82a32ec3311bbff70a0
Pranshu Budhrani <pranshubudhrani@gmail.com>
2021-12-12 16:12:45 +0530

    Initial Commit

---
+++ BharatCare/BharatCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BharatCareApp.swift
+//  BharatCare
+//
+//  Created by Pranshu Budhrani on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BharatCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
